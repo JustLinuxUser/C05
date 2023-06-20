a8e00bc6f2c978365f8fbe540a98e6307c506f2d
Andriy Dohniak <pavlofort@gmail.com>
2023-06-20 20:09:03 +0200

    a commit

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adokhnia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 13:45:58 by adokhnia          #+#    #+#             */
+/*   Updated: 2023/06/12 13:46:00 by adokhnia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	result = nb;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	if (nb > 1)
+	{
+		nb--;
+		result *= ft_recursive_factorial(nb);
+	}
+	return (result);
+}
